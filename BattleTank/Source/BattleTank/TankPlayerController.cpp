4e0e0c2167b5ec7675717d74287d1c078642f6d7
Pacheco <rikardo_m17@hotmail.com>
2016-10-20 17:27:41 +0100

    aula 114
    created controller and tank classes

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+/*ATank* UTankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}*/
+
+
+
