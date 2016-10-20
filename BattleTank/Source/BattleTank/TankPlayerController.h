c8aeb317f817dd0a197cefc8b2f98a5c781cb3d1
Pacheco <rikardo_m17@hotmail.com>
2016-10-20 19:17:22 +0100

    aula 121 - LookDirection implemented

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -32,4 +32,7 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float CrossHairYLocation = 0.33333;
+
+	// Return out parameter
+	bool LookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 };
